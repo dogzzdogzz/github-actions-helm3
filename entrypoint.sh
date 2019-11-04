58e7edc9b19bf61f162c3fa8b69b38458c840225
Tony Lee <tony.lee@shopback.com>
2019-11-04 17:45:18 +0800

    fix action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 printf ${KUBECONFIG_FILE} > /.kube/config.yaml
 export KUBECONFIG=/.kube/config.yaml
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm install --namespace ${INPUT_NAMESPACE} ${INPUT_RELEASE} ${INPUT_CHART}
