b5c424f2a01a0803fc1242fdf1c1a0602a9448de
Tony Lee <tony.lee@shopback.com>
2019-11-06 18:16:07 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "${KUBECONFIG_FILE}" > /root/kubeconfig.yaml
 export KUBECONFIG=/root/kubeconfig.yaml
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+# helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 VALUES_TEMPLATE="${INPUT_CHART}/values.yaml"
 if [ "${INPUT_VALUES_FILES}" != "" ]; then
     VALUES_TEMPLATE="${VALUES_TEMPLATE} ${INPUT_VALUES_FILES}"
