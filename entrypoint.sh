043b39b3b0b438caa952954d8416133ffa191f78
Tony Lee <tony.lee@shopback.com>
2019-11-06 16:30:11 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ echo "${KUBECONFIG_FILE}" > /root/kubeconfig.yaml
 export KUBECONFIG=/root/kubeconfig.yaml
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 VALUES_TEMPLATE="${INPUT_CHART}/values.yaml"
-if [ "${INPUT_ENV_FILES}" != "" ]; then
-    VALUES_TEMPLATE="${VALUES_TEMPLATE} ${INPUT_ENV_FILES}"
+if [ "${INPUT_VALUES_FILES}" != "" ]; then
+    VALUES_TEMPLATE="${VALUES_TEMPLATE} ${INPUT_VALUES_FILES}"
 fi
 
 echo ${VALUES_TEMPLATE} | xargs printf -- "-f ${GITHUB_WORKSPACE}/%s\n" | xargs echo helm template ${INPUT_RELEASE} ${GITHUB_WORKSPACE}/${INPUT_CHART} --namespace ${INPUT_NAMESPACE}
