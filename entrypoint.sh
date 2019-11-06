b39d13d116db0cd1f5dd9ecb921184c7c22ac37e
Tony Lee <tony.lee@shopback.com>
2019-11-06 16:44:57 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 set -e
-# helm upgrade --values values.yaml --set $set_overrides \
-#      $INPUT_NAME $INPUT_CHART_NAME
+
 echo "${KUBECONFIG_FILE}" > /root/kubeconfig.yaml
 export KUBECONFIG=/root/kubeconfig.yaml
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
